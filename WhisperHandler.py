db1de279a058587807b6aefb53797b45814a8d30
Tim <Thiele@iwd-marketresearch.de>
2024-02-10 18:02:11 +0100

    Finished GUI, not bug tested yet

---
+++ WhisperHandler.py
@@ -0,0 +1,112 @@
+# === IMPORTS ===
+import whisper
+import time
+import librosa
+import soundfile as sf
+import re
+import os
+import shutil
+
+# === UTIL FUNCTIONS ===
+def encode_string(s, encoding='charmap'):
+    try:
+        # Attempt to encode the string using the specified encoding
+        encoded_s = s.encode(encoding)
+    except UnicodeEncodeError as e:
+        # If a UnicodeEncodeError is encountered, extract the error position
+        error_position = e.start  # This gives you the position of the error
+        print(f"Error encoding character at position {error_position}: {s[error_position]}")
+        return None, error_position
+    return encoded_s, None
+
+def replace_character_at_position(original_string, position, replacement=''):
+    if position < 0 or position >= len(original_string):
+        raise ValueError("Position is out of the string's bounds")
+    return original_string[:position] + replacement + original_string[position+1:]
+
+
+class Whisper_Handler:
+    def __init__(self, file_root, language="de", model_size="small"):
+        self.file_root = file_root
+        language_options = ["en", "de"]
+        model_size_options = ["tiny", "base", "small", "medium", "large"]
+        self.model = whisper.load_model(model_size)
+        whisper.DecodingOptions(language=language)
+        
+        self.folders =  {
+            "root": r"WhisperAudio",
+            "raw": r"WhisperAudio\FilesToTranscribe",
+            "processed": r"WhisperAudio\ProcessedAudio",
+            "transcribed": r"WhisperAudio\TextFiles",
+        }
+        for key, value in self.folders.items():
+            self.folders[key] = os.path.join(self.file_root, value)
+            os.makedirs(self.folders[key], exist_ok=True)
+   
+
+    def transcribe(self):
+        audio_folder, processed_folder, text_file_folder = self.folders["raw"], self.folders["processed"], self.folders["transcribed"]
+        audio_files, audio_names = [], []
+        for file in os.listdir(audio_folder):
+          if file.endswith(".m4a") or file.endswith(".mp3"):
+            audio_files.append(os.path.join(audio_folder, file))
+            audio_names.append(file)
+
+        for f in audio_files:
+          print(f)
+
+        if len(audio_files) == 0:
+          print("You have no files.")
+
+        # Loop through the audio files, split each audio file based on pauses in speech then transcribe them with Whisper.
+        for i, file in enumerate(audio_files):
+          print(f"Processing {audio_names[i]}...")
+          # Load the audio file and convert it to 16 kHz mono
+          audio, sr = librosa.load(file, sr=16000, mono=True)
+          # Detect pauses and split the audio. We use a threshold of -30 dB and a minimum pause length of 0.5 seconds.
+          pauses = librosa.effects.split(audio, top_db=30, frame_length=2048, hop_length=128)
+          # Transcribe each segment and concatenate the results
+          transcription = ""
+          for start, end in pauses: # For each segment
+            segment = audio[start:end]
+            # Save the segment as a temporary wav file
+            temp_file = os.path.join(self.file_root, "temp.wav")
+            sf.write(temp_file, segment, sr, subtype='PCM_16')
+            if os.path.getsize(temp_file) > 10000:
+              # Transcribe the segment with Whisper
+              if os.path.exists(temp_file):
+                print(f"The file '{temp_file}' exists.") # DEBUG CHECK
+              temp_audio, sr = librosa.load(temp_file, sr=16000, mono=True)
+              result = self.model.transcribe(temp_audio)
+              text = result["text"]
+              # Append the text to the transcription
+              print(len(transcription.split(" ")), "words processed")
+              transcription += text.strip() + " "
+              # Delete the temporary file
+              os.remove(temp_file)
+          # Print the transcription
+          print(f"Transcription of {audio_names[i]}:\n")
+          print(transcription)
+          print("\n")
+
+          # Convert the spaces between sections into paragraph breaks and save the transcription as a txt document in the same folder as MyAudio.
+          transcription = re.sub(r"\s\s+", "\n\n", transcription) # Replace multiple spaces with newlines
+          text_file = os.path.join(text_file_folder, audio_names[i][:-4] + ".txt") # Create the text file name
+          
+          # QS valid characters
+          encoded_transcription = None
+          while encoded_transcription == None:
+              encoded_transcription, error_position = encode_string(transcription)
+              if not error_position is None:
+                transcription = replace_character_at_position(transcription, error_position, replacement="?")
+
+          with open(text_file, "w") as f: # Write the transcription to the text file
+            f.write(transcription)
+          print(f"Saved transcription as {text_file}")
+
+        # Move the audio files to processed folder
+        if not os.path.exists(processed_folder): # Create the folder if it does not exist
+          os.mkdir(processed_folder)
+        for file in audio_files: # Move each audio file to the processed folder
+          shutil.move(file, os.path.join(processed_folder, os.path.basename(file)))
+          print(f"Moved {file} to {processed_folder}")
--- tkinter_wrapper.py
+++ tkinter_wrapper.py
@@ -1,7 +1,33 @@
 # https://github.com/TomSchimansky/CustomTkinter
 # https://customtkinter.tomschimansky.com/
 
+from WhisperHandler import Whisper_Handler
 import customtkinter
+import os, json
+
+
+class MyRadiobuttonFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.radiobuttons = []
+        self.variable = customtkinter.StringVar(value="")
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            radiobutton = customtkinter.CTkRadioButton(self, text=value, value=value, variable=self.variable)
+            radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.radiobuttons.append(radiobutton)
+
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
 
 
 class App(customtkinter.CTk):
@@ -16,13 +42,21 @@ class App(customtkinter.CTk):
         self.geometry("400x240")
 
         self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        checkbox_1 = customtkinter.CTkCheckBox(self, text="German")
-        checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
-        checkbox_2 = customtkinter.CTkCheckBox(self, text="English")
-        checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        with open("languages.json", 'r', encoding='utf-8') as file:
+            whisper_languages = json.load(file)
+            whisper_languages = {value: key for key, value in whisper_languages.items()}
 
-        button = customtkinter.CTkButton(master=self, text="Transcribe audio", command=self.button_callback)
+        self.radiobutton_frame = MyRadiobuttonFrame(self, "Choose spoken language", values=whisper_languages)
+        self.radiobutton_frame.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
+
+        chosen_language = self.radiobutton_frame.get()
+        file_root = os.path.expanduser("~/Documents/Whisper/Data")
+        # r"C:\Users\TThiele\Documents\Whisper\Data"
+        wh = Whisper_Handler(file_root, chosen_language)
+
+        button = customtkinter.CTkButton(master=self, text="Transcribe", command=self.button_callback)
         button.grid(row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 
 
