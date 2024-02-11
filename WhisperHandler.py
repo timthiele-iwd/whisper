4cf6b4f066cdad75ff969557d7c7caedbac218d0
Tim <Thiele@iwd-marketresearch.de>
2024-02-11 15:04:04 +0100

    Working version of whisper app

--- WhisperHandler.py
+++ WhisperHandler.py
@@ -26,7 +26,7 @@ def replace_character_at_position(original_string, position, replacement=''):
 
 
 class Whisper_Handler:
-    def __init__(self, file_root, language="de", model_size="small"):
+    def __init__(self, file_root, language="de", model_size="medium"):
         self.file_root = file_root
         language_options = ["en", "de"]
         model_size_options = ["tiny", "base", "small", "medium", "large"]
@@ -45,68 +45,78 @@ class Whisper_Handler:
    
 
     def transcribe(self):
-        audio_folder, processed_folder, text_file_folder = self.folders["raw"], self.folders["processed"], self.folders["transcribed"]
-        audio_files, audio_names = [], []
-        for file in os.listdir(audio_folder):
-          if file.endswith(".m4a") or file.endswith(".mp3"):
-            audio_files.append(os.path.join(audio_folder, file))
-            audio_names.append(file)
+        
+        try:
+          audio_folder, processed_folder, text_file_folder = self.folders["raw"], self.folders["processed"], self.folders["transcribed"]
+          audio_files, audio_names = [], []
+          for file in os.listdir(audio_folder):
+            if file.endswith(".m4a") or file.endswith(".mp3"):
+              audio_files.append(os.path.join(audio_folder, file))
+              audio_names.append(file)
 
-        for f in audio_files:
-          print(f)
+          for f in audio_files:
+            print(f)
 
-        if len(audio_files) == 0:
-          print("You have no files.")
+          if len(audio_files) == 0:
+            print("You have no files.")
+            return f"No files found in the specified directory {audio_folder}"
 
-        # Loop through the audio files, split each audio file based on pauses in speech then transcribe them with Whisper.
-        for i, file in enumerate(audio_files):
-          print(f"Processing {audio_names[i]}...")
-          # Load the audio file and convert it to 16 kHz mono
-          audio, sr = librosa.load(file, sr=16000, mono=True)
-          # Detect pauses and split the audio. We use a threshold of -30 dB and a minimum pause length of 0.5 seconds.
-          pauses = librosa.effects.split(audio, top_db=30, frame_length=2048, hop_length=128)
-          # Transcribe each segment and concatenate the results
-          transcription = ""
-          for start, end in pauses: # For each segment
-            segment = audio[start:end]
-            # Save the segment as a temporary wav file
-            temp_file = os.path.join(self.file_root, "temp.wav")
-            sf.write(temp_file, segment, sr, subtype='PCM_16')
-            if os.path.getsize(temp_file) > 10000:
-              # Transcribe the segment with Whisper
-              if os.path.exists(temp_file):
-                print(f"The file '{temp_file}' exists.") # DEBUG CHECK
-              temp_audio, sr = librosa.load(temp_file, sr=16000, mono=True)
-              result = self.model.transcribe(temp_audio)
-              text = result["text"]
-              # Append the text to the transcription
-              print(len(transcription.split(" ")), "words processed")
-              transcription += text.strip() + " "
-              # Delete the temporary file
-              os.remove(temp_file)
-          # Print the transcription
-          print(f"Transcription of {audio_names[i]}:\n")
-          print(transcription)
-          print("\n")
+          # Loop through the audio files, split each audio file based on pauses in speech then transcribe them with Whisper.
+          for i, file in enumerate(audio_files):
+            print(f"Processing {audio_names[i]}...")
+            # Load the audio file and convert it to 16 kHz mono
+            audio, sr = librosa.load(file, sr=16000, mono=True)
+            # Detect pauses and split the audio. We use a threshold of -30 dB and a minimum pause length of 0.5 seconds.
+            pauses = librosa.effects.split(audio, top_db=30, frame_length=2048, hop_length=128)
+            # Transcribe each segment and concatenate the results
+            transcription = ""
+            for start, end in pauses: # For each segment
+              segment = audio[start:end]
+              # Save the segment as a temporary wav file
+              temp_file = os.path.join(self.file_root, "temp.wav")
+              sf.write(temp_file, segment, sr, subtype='PCM_16')
+              if os.path.getsize(temp_file) > 10000:
+                # Transcribe the segment with Whisper
+                if os.path.exists(temp_file):
+                  print(f"The file '{temp_file}' exists.") # DEBUG CHECK
+                temp_audio, sr = librosa.load(temp_file, sr=16000, mono=True)
+                result = self.model.transcribe(temp_audio)
+                text = result["text"]
+                # Append the text to the transcription
+                print(len(transcription.split(" ")), "words processed")
+                transcription += text.strip() + " "
+                # Delete the temporary file
+                os.remove(temp_file)
+            # Print the transcription
+            print(f"Transcription of {audio_names[i]}:\n")
+            print(transcription)
+            print("\n")
 
-          # Convert the spaces between sections into paragraph breaks and save the transcription as a txt document in the same folder as MyAudio.
-          transcription = re.sub(r"\s\s+", "\n\n", transcription) # Replace multiple spaces with newlines
-          text_file = os.path.join(text_file_folder, audio_names[i][:-4] + ".txt") # Create the text file name
-          
-          # QS valid characters
-          encoded_transcription = None
-          while encoded_transcription == None:
-              encoded_transcription, error_position = encode_string(transcription)
-              if not error_position is None:
-                transcription = replace_character_at_position(transcription, error_position, replacement="?")
+            # Convert the spaces between sections into paragraph breaks and save the transcription as a txt document in the same folder as MyAudio.
+            transcription = re.sub(r"\s\s+", "\n\n", transcription) # Replace multiple spaces with newlines
+            text_file = os.path.join(text_file_folder, audio_names[i][:-4] + ".txt") # Create the text file name
+            
+            # QS valid characters
+            encoded_transcription = None
+            while encoded_transcription == None:
+                encoded_transcription, error_position = encode_string(transcription)
+                if not error_position is None:
+                  transcription = replace_character_at_position(transcription, error_position, replacement="?")
 
-          with open(text_file, "w") as f: # Write the transcription to the text file
-            f.write(transcription)
-          print(f"Saved transcription as {text_file}")
+            with open(text_file, "w") as f: # Write the transcription to the text file
+              f.write(transcription)
+            print(f"Saved transcription as {text_file}")
 
-        # Move the audio files to processed folder
-        if not os.path.exists(processed_folder): # Create the folder if it does not exist
-          os.mkdir(processed_folder)
-        for file in audio_files: # Move each audio file to the processed folder
-          shutil.move(file, os.path.join(processed_folder, os.path.basename(file)))
-          print(f"Moved {file} to {processed_folder}")
+          # Move the audio files to processed folder
+          if not os.path.exists(processed_folder): # Create the folder if it does not exist
+            os.mkdir(processed_folder)
+          for file in audio_files: # Move each audio file to the processed folder
+            shutil.move(file, os.path.join(processed_folder, os.path.basename(file)))
+            print(f"Moved {file} to {processed_folder}")
+
+        except Exception as e:
+            print("Exception occured: ", e)
+            return f"An error occured: {e}"
+        
+        else:
+            return "Successfully transcribed"
--- tkinter_wrapper.py
+++ tkinter_wrapper.py
@@ -3,10 +3,45 @@
 
 from WhisperHandler import Whisper_Handler
 import customtkinter
-import os, json
+import os, sys, json
 
 
-class MyRadiobuttonFrame(customtkinter.CTkFrame):
+class ToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(self, master=None, chosen_language=None, whisper_languages=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.master = master
+        self.chosen_language = chosen_language
+        self.whisper_languages = whisper_languages
+        self.title("Running transcription")
+        self.geometry("300x400")
+        self.label = customtkinter.CTkLabel(self, text=f"Chosen language: {chosen_language}")
+        self.label.pack(padx=20, pady=20)
+        self.label = customtkinter.CTkLabel(self, text=f"Transcription is running ...")
+        self.label.pack(padx=20, pady=20)
+        self.after(3000, self.start_whisper_handler)
+
+    def button_callback(self):
+        print("Top level window closed.")
+        self.destroy()
+        print("Main window closed.")
+        self.master.destroy()
+    
+    def start_whisper_handler(self):
+        if self.chosen_language is not None:
+            chosen_language_abbr = self.whisper_languages[self.chosen_language]
+            file_root = os.path.expanduser("~/Documents/Whisper/Data")
+            print("Starting transcription with whisper.")
+            wh = Whisper_Handler(file_root, chosen_language_abbr)
+            state = wh.transcribe()
+            
+        self.label = customtkinter.CTkLabel(self, text=state)
+        self.label.pack(padx=20, pady=20)
+
+        button = customtkinter.CTkButton(master=self, text="Close", command=self.button_callback)
+        button.pack(padx=20, pady=20)
+
+
+class ScrollableRadiobuttonFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master, title, values):
         super().__init__(master)
         self.grid_columnconfigure(0, weight=1)
@@ -15,8 +50,8 @@ class MyRadiobuttonFrame(customtkinter.CTkFrame):
         self.radiobuttons = []
         self.variable = customtkinter.StringVar(value="")
 
-        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
-        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
 
         for i, value in enumerate(self.values):
             radiobutton = customtkinter.CTkRadioButton(self, text=value, value=value, variable=self.variable)
@@ -25,7 +60,6 @@ class MyRadiobuttonFrame(customtkinter.CTkFrame):
 
     def get(self):
         return self.variable.get()
-
     def set(self, value):
         self.variable.set(value)
 
@@ -34,34 +68,47 @@ class App(customtkinter.CTk):
     
     def __init__(self):
         super().__init__()
+        
+        self.chosen_language = None
+        self.toplevel_window = None
 
         customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
         customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
         self.title("Whisper App")
-        self.geometry("400x240")
+        self.geometry("300x400")
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         with open("languages.json", 'r', encoding='utf-8') as file:
             whisper_languages = json.load(file)
-            whisper_languages = {value: key for key, value in whisper_languages.items()}
+            self.whisper_languages = {value: key for key, value in whisper_languages.items()}
 
-        self.radiobutton_frame = MyRadiobuttonFrame(self, "Choose spoken language", values=whisper_languages)
-        self.radiobutton_frame.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
-
-        chosen_language = self.radiobutton_frame.get()
-        file_root = os.path.expanduser("~/Documents/Whisper/Data")
-        # r"C:\Users\TThiele\Documents\Whisper\Data"
-        wh = Whisper_Handler(file_root, chosen_language)
+        self.radiobutton_frame = ScrollableRadiobuttonFrame(self, "Choose spoken language", values=self.whisper_languages)
+        self.radiobutton_frame.grid(row=0, column=0, padx=(0, 10), pady=(10, 0), sticky="w")
 
         button = customtkinter.CTkButton(master=self, text="Transcribe", command=self.button_callback)
-        button.grid(row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        button.grid(row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=2)      
 
 
     def button_callback(self):
-        print("button pressed")
+        print("Transcribe button pressed.")
+        self.open_toplevel()
+
+
+    def open_toplevel(self):
+        chosen_language = self.radiobutton_frame.get()
+        print(f"Chosen language: {chosen_language}.")
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = ToplevelWindow(
+                self, 
+                chosen_language=chosen_language, 
+                whisper_languages=self.whisper_languages
+            )  # create window if its None or destroyed
+            self.toplevel_window.focus()
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
 
 
 app = App()
